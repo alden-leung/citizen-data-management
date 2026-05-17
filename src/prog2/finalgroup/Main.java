8cc7c83cad69707ad774c4c6ba4647b7d607ee4b
Ernst Cimatu <elic2262893@slu.edu.ph>
2026-05-17 23:10:45 +0800

    Official entry point, launches the program

--- src/prog2/finalgroup/Main.java
+++ src/prog2/finalgroup/Main.java
@@ -1,4 +1,15 @@
 package prog2.finalgroup;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Gui().setVisible(true);
+            }
+        });
+    }
 }
